f66284fc49b2f4251ed9934427b513b66927a096
TheJadePhoenix <114638540+TheJadePhoenix@users.noreply.github.com>
2023-03-19 18:59:57 -0400

    Modified Card Prefab
    Improved Shadow in Card Prefab

--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -6,10 +6,13 @@ using UnityEngine.Animations;
 public class Shadow : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _spriteRenderer, _parentSpriteRenderer;
+    [SerializeField] Transform _cardTransform;
 
     void Update()
     {
         _spriteRenderer.flipX = _parentSpriteRenderer.flipX;
         _spriteRenderer.sprite = _parentSpriteRenderer.sprite;
+
+        transform.rotation = Quaternion.identity;
     }
 }
